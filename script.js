2b7414506301d3414b1a7bb94708402f9cddc785
mohammed kaif <mkaif555123@gmail.com>
2024-05-13 00:03:45 +0500

    100% WORK DONE

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let clouds = document.getElementById("clouds");
 let humidity = document.getElementById("humidity");
 let pressure = document.getElementById("pressure");
 let form = document.querySelector("form");
+let main = document.querySelector("main");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -25,9 +26,23 @@ const searchWeather = () => {
       console.log(data);
       if (data.cod == 200) {
         city.querySelector("figcaption").innerHTML = data.name;
-        city.querySelector("img").src = 'https://flagsapi.com/'+data.sys.country+'/shiny/32.png'
-      
-        temperature.querySelector("img").src = 'http:openweathermap.org/img/wn/'+data.weather[0].icon+'@4x.png'
-    }
+        city.querySelector("img").src =
+          "https://flagsapi.com/" + data.sys.country + "/shiny/32.png";
+
+        temperature.querySelector("img").src =
+          "http:openweathermap.org/img/wn/" + data.weather[0].icon + "@4x.png";
+        temperature.querySelector("figcaption span").innerText = data.main.temp;
+        description.innerHTML = data.weather[0].description;
+        clouds.innerHTML = data.clouds.all;
+        humidity.innerText = data.main.humidity;
+        pressure.innerText = data.main.pressure;
+      } else {
+        //false
+        main.classList.add("error");
+        setTimeout(() => {
+            main.classList.remove("error")
+        }, 1000);
+      }
+      getInput.value = " ";
     });
 };
